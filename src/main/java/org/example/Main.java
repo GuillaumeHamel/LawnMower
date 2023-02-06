30c122dad4b25fe341af144f0f70394a88e14add
Guillaume Hamel <guillaume.hamel@carbon-it.com>
2023-02-06 15:42:39 +0100

    Finalized project integration by wiring all components in main class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,16 @@
 package org.example;
 
+import org.example.fileUtils.FileHandler;
+import org.example.fileUtils.LawnParser;
+import org.example.fileUtils.MowerFormatter;
+
 public class Main {
+    private static final String filePath = "src/main/resources/example.txt";
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        final var initialLawn = LawnParser.parseToLawn(FileHandler.readLines(filePath));
+        final var finalLawn = initialLawn.mow();
+        FileHandler.writeToFile(MowerFormatter.mowerToString(finalLawn), filePath.split("\\.")[0] + "[result].txt");
     }
+
 }
